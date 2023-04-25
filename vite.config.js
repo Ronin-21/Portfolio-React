4d0a558f1f886dedb8893d6b5dc8c3e8e7537ee8
Abel Alejandro Acuña Lasarte <83144872+Ronin-21@users.noreply.github.com>
2023-04-24 21:58:07 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+		assetsInlineLimit: 0,
+		rollupOptions: {
+			input: {
+				main: 'index.html',
+				pdf: 'src/Assets/Abel_Acuña-CV.pdf',
+				social: 'src/Assets/img/Portada.png',
+			},
+		},
+	},
 })
